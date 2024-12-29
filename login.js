728662a5bccb02f51807531a4feeb8891af83dee
khitish-ztx <khitish152006@gmail.com>
2024-12-29 09:53:36 +0530

    hello

--- login.js
+++ login.js
@@ -1,37 +1,50 @@
+let pop = document.querySelector(".pop");
+let popbtn = document.querySelector("#popbtn");
+let user = document.getElementById("username");
+let pass = document.getElementById("password");
+let userError = document.getElementById("userError");
+let passError = document.getElementById("passError");
+const realuser=1234
+const realpass=69
 
+function login() {
+    let hasError = false;
 
-let realuser=1234;
-let realpass=1234;
-
-function login(){
-    let user=document.getElementById("username").value
-    let pass=document.getElementById("password").value
-    let pop=document.querySelector(".pop")
-    let popbtn=document.querySelector("#popbtn")
-    if(user==realuser && pass==realpass){
-        pop.style.visibility="visible"
-        popbtn.addEventListener("click",function(){
-            window.location="sample.html"
-
-        })
-
-
-
-
-        
+    if (user.value.trim() === '') {
+        userError.style.display = "block";
+        userError.innerHTML = "This field is required";
+        hasError = true;
+    } else {
+        userError.style.display = "none";
     }
-    else{
-        let popitem=document.querySelector("#popmsg")
-        let tickplay=document.querySelector(".play")
-        tickplay.innerHTML="<h1 color='red'> Error Fetching data!! <h1>"
-        popitem.innerHTML="Incorrect userame and password for this Account"
-        popbtn.innerHTML="Try Again"
-        pop.style.visibility="visible"
-        popbtn.addEventListener("click",function(){
-            window.location="login.html"
-            
-        })
 
-        
+    if (pass.value.trim() === '') {
+        passError.style.display = "block";
+        passError.innerHTML = "This field is required";
+        hasError = true;
+    } else {
+        passError.style.display = "none";
+    }
+
+    if (hasError) {
+        return;
+    }
+
+    if (user.value == realuser && pass.value == realpass) {
+        pop.style.visibility = "visible";
+        popbtn.addEventListener("click", function() {
+            window.location = "sample.html";
+        });
+    } 
+    else {
+        let popitem = document.querySelector("#popmsg");
+        let tickplay = document.querySelector(".play");
+        tickplay.innerHTML = "<h1 style='color:red;'> Error Fetching data!! <h1>";
+        popitem.innerHTML = "Incorrect username and password for this Account";
+        popbtn.innerHTML = "Try Again";
+        pop.style.visibility = "visible";
+        popbtn.addEventListener("click", function() {
+            window.location = "login.html";
+        });
     }
 }
